5c26620e58ec0d6b209717207db5409b384e7451
Alokjha476 <alokjha476@gmail.com>
2023-08-13 14:10:47 +0530

    first commit

---
+++ src/Alpha4Batch/String/Count.java
@@ -0,0 +1,21 @@
+package Alpha4Batch.String;
+
+public class Count {
+
+    public static void count(String s){
+
+        int count = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+    public static void main(String[] args) {
+        String s = "ApnaCollege";
+        int idx = 5;
+        count(s);
+
+
+
+    }
+}
